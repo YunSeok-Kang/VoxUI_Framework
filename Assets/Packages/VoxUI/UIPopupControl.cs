740b9cf7d287d9d192399083d3e2d54184cacc31
YSKang <ysck73@naver.com>
2019-09-18 21:08:38 +0900

    UIPopupControl 클래스 제작

---
+++ Assets/Packages/VoxUI/UIPopupControl.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(VoxUIObjectBlackBorad))]
+public class UIPopupControl : MonoBehaviour
+{
+    private VoxUIObjectBlackBorad _uiObjBB = null;
+
+    /// <summary>
+    /// 현재 선택된 오브젝트의 uiObjects 리스트 기준의 인덱스
+    /// </summary>
+    public int SelectedIndex
+    {
+        internal set; get;
+    }
+
+    /// <summary>
+    /// 현재 선택된 오브젝트의 uiObjects 리스트 기준의 오브젝트 참조 값
+    /// </summary>
+    public VoxUIObjectInfo SelectedUIObj
+    {
+        internal set; get;
+    }
+
+    // ----------------------------------------------------------------------- Unity Evenets ----------------------------------------------------------------------- //
+
+    private void Awake()
+    {
+        if (_uiObjBB == null)
+        {
+            _uiObjBB = GetComponent<VoxUIObjectBlackBorad>();
+        }
+    }
+
+    // -------------------------------------------------------------------------- Methods -------------------------------------------------------------------------- //
+
+    //public void SelectUI(string key)
+    //{
+    //    VoxUIObjectInfo currentUIObj = _uiObjBB.GetUIObjectInfoByKey(key);
+    //    if (currentUIObj == null)
+    //    {
+    //        Debug.Log("없는 키");
+    //        return;
+    //    }
+
+
+    //    if (SelectedUIObj != currentUIObj)
+    //    {
+    //        SelectedUIObj = currentUIObj;
+
+    //        ShowSelectedUI(SelectedUIObj);
+    //    }
+    //    else
+    //    {
+    //        Debug.Log("UIPopupControl: 중복된 오브젝트 선택");
+    //    }
+
+    //    //try
+    //    //{
+    //    //    currentUIObj = _uiObjBB.GetUIObjectInfoByKey(key);
+
+    //    //    if (SelectedUIObj != currentUIObj)
+    //    //    {
+    //    //        SelectedUIObj = currentUIObj;
+    //    //        Debug.Log("선택: " + SelectedUIObj);
+    //    //    }
+    //    //    else
+    //    //    {
+    //    //        Debug.Log("중복: " + SelectedUIObj);
+    //    //    }
+    //    //}
+    //    //catch (System.NullReferenceException nullException)
+    //    //{
+    //    //    Debug.Log(nullException);
+
+    //    //    return;
+    //    //}
+    //}
+
+    public void SelectPopupUI(string key)
+    {
+        VoxUIObjectInfo[] objectInfos = _uiObjBB.GetUIObjectInfosToArray();
+
+        foreach (VoxUIObjectInfo currentUIObject in objectInfos)
+        {
+            if (currentUIObject.uiObjectKey.Equals(key))
+            {
+                currentUIObject.uiObject.SetActive(true);
+                SelectedUIObj = currentUIObject;
+            }
+            else
+            {
+                currentUIObject.uiObject.SetActive(false);
+            }
+        }
+    }
+
+    public void ClearPopup()
+    {
+        SelectedUIObj.uiObject.SetActive(false);
+        SelectedUIObj = null;
+    }
+}
---
+++ Assets/Packages/VoxUI/VoxUIObjectBlackBorad.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VoxUIObjectBlackBorad : MonoBehaviour
+{
+    [SerializeField]
+    private List<VoxUIObjectInfo> uiObjects = new List<VoxUIObjectInfo>();
+
+    /// <summary>
+    /// key를 이용하여 uiObjects에 등록된 UI 오브젝트 검색
+    /// </summary>
+    /// <param name="key"></param>
+    /// <returns> 
+    ///     성공: 찾은 오브젝트의 uiObjects 리스트 기준 index 반환
+    ///     실패: -1 반환
+    /// </returns>
+    public int FindtUIObjectIndex(string key)
+    {
+        int iterationIndex = 0;
+
+        foreach (VoxUIObjectInfo objectInfo in uiObjects)
+        {
+            if (objectInfo.uiObjectKey.Equals(key))
+            {
+                return iterationIndex;
+            }
+
+            ++iterationIndex;
+        }
+
+        return -1;
+    }
+
+    /// <summary>
+    /// index를 이용하여 uiObjects에 등록된 UIObjectInfo 객체 반환
+    /// </summary>
+    /// <param name="index"></param>
+    /// <returns> 검색된 VoxUIObjectInfo </returns>
+    public VoxUIObjectInfo GetUIObjectInfoByIndex(int index)
+    {
+        // uiObjects가 가지고 있는 오브젝트 갯수보다 더 큰 index가 들어오면 검색 중지
+        if (index >= uiObjects.Count)
+        {
+            return null;
+        }
+
+        return uiObjects[index];
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="key"></param>
+    /// <returns></returns>
+    public VoxUIObjectInfo GetUIObjectInfoByKey(string key)
+    {
+        foreach (VoxUIObjectInfo objectInfo in uiObjects)
+        {
+            if (objectInfo.uiObjectKey.Equals(key))
+            {
+                return objectInfo;
+            }
+        }
+
+        return null;
+    }
+
+    /// <summary>
+    /// 리스트 내에 있는 VoxUIObjectInfo 데이터를 배열로 변환하여 반환 
+    /// </summary>
+    /// <returns></returns>
+    public VoxUIObjectInfo[] GetUIObjectInfosToArray()
+    {
+        return uiObjects.ToArray();
+    }
+}
