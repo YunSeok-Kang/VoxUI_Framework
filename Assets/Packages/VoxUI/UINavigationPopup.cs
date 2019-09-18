22347070b98525235c5a94690326a613c89bef84
YSKang <ysck73@naver.com>
2019-09-18 22:15:46 +0900

    UINavigationPopup 추가

---
+++ Assets/Packages/VoxUI/UINavigationPopup.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(VoxUIObjectBlackBorad))]
+public class UINavigationPopup : MonoBehaviour
+{
+    private VoxUIObjectBlackBorad _uiObjBB = null;
+
+    Stack<GameObject> _uiNavigationStack = null;
+
+    // ----------------------------------------------------------------------- Unity Evenets ----------------------------------------------------------------------- //
+
+    private void Awake()
+    {
+        _uiNavigationStack = new Stack<GameObject>();
+
+        if (_uiObjBB == null)
+        {
+            _uiObjBB = GetComponent<VoxUIObjectBlackBorad>();
+        }
+    }
+
+    // -------------------------------------------------------------------------- Methods -------------------------------------------------------------------------- //
+
+    public void PushUI(string key)
+    {
+        VoxUIObjectInfo selectedUIObjInfo = _uiObjBB.GetUIObjectInfoByKey(key);
+        GameObject selectedUIObject = selectedUIObjInfo.uiObject;
+
+        _uiNavigationStack.Push(selectedUIObjInfo.uiObject);
+        selectedUIObject.SetActive(true);
+    }
+
+    public void PopUI()
+    {
+        GameObject poppingUIObj = _uiNavigationStack.Pop();
+        poppingUIObj.SetActive(false);
+    }
+}
