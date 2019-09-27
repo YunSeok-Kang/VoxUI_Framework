4aec66dea2cd9865525738b54e5676fc82b9fe89
YSKang <ysck73@naver.com>
2019-09-28 01:09:08 +0900

    ENH: LongClick 버튼, 버튼을 눌리고 있을 시 이벤트를 검출하는 기능 추가
    - UIButtonStayEvent: UI Button 컴포넌트에 추가하여 사용. 버튼을 눌리고 있으면 UI Button에
     등록된 OnClick 이벤트 호출
    - UILongClickButton: 버튼을 일정 기간 이상 누르고 있으면 클릭 이벤트 발동

---
+++ Assets/Packages/VoxUI/UIButtonStayEvent.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+ 
+public class UIButtonStayEvent : EventTrigger
+{
+    private Coroutine _routine = null;
+
+    public override void OnPointerDown(PointerEventData data)
+    {
+        _routine = StartCoroutine(Routine());
+    }
+
+    public IEnumerator Routine()
+    {
+        while (this.enabled)
+        {
+            GetComponent<Button>().onClick.Invoke();
+            yield return null;
+        }
+    }
+
+    public override void OnPointerUp(PointerEventData data)
+    {
+        StopCoroutine(_routine);
+    }
+}
